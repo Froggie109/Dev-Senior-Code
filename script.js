51a332c050cc511a11285237f29f028dee301c99
FroggieColors <160804930+Froggie109@users.noreply.github.com>
2024-10-16 22:45:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function redirect() {
+    window.open("https://discord.gg/MQyHSPT3fH", "_blank");  // Team Python
+    setTimeout(function() {
+        window.open("https://discord.gg/auUGb6EjcU", "_blank");  // Team JAVA
+    }, 1000);  // Retraso de 1 segundo
+}
